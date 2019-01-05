5fcbe9ddb082335a84e2279383f4d3805973de4e
sxarp <you@example.com>
2019-01-05 20:38:59 +0900

    $ be rubocop --auto-correct

--- db/Schemafile.rb
+++ db/Schemafile.rb
@@ -1,6 +1,6 @@
 create_table "articles", force: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
   t.string   "title"
-  t.text   "content"
+  t.text "content"
   t.integer  "status"
   t.datetime "created_at"
   t.datetime "updated_at"
